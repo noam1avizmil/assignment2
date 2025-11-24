29e9d9fab0846f7c0a5d4be4b1faf4e7b27f73d4
“Noam1avizmil” <noamav10612@gmail.com>
2025-11-24 16:15:56 +0200

    fixing

--- Pokedex.c
+++ Pokedex.c
@@ -5,16 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#iclude "Defs.h""
+#include "Defs.h"
 
 ResultStatus main(int argc, char* argv[]) {
-    numberofTypes = atoi(argv[1];)
-    numberofPokemons = atoi(argv[2];)
-    File config_file = fopen(argv[3], "r");
+    int numberofTypes = atoi(argv[1]);
+    int numberofPokemons = atoi(argv[2]);
+    FILE * config_file = fopen(argv[3], "r");
     if (config_file == NULL){return failure;}
     char buffer[300];
     fgets(buffer, sizeof(buffer), config_file);
-    PokemonList* types = (pokemonList*)malloc(numberofTypes*sizeof(pokemonList));
+    PokemoneList* types = (PokemoneList*)malloc(numberofTypes*sizeof(PokemoneList));
     Pokemone* pokemons = (Pokemone*)malloc(numberofPokemons*sizeof(Pokemone));
     fgets(buffer,sizeof(buffer), config_file);
     char* curr_type = strtok(buffer,",\n"); //array of types
